773887aeed1ed728a3757b6d37164f6389472207
iamflowz <dakota.lewallen17@gmail.com>
2018-10-23 19:22:56 -0400

    first iteration of cord input to motor outputs conversion algo

--- Backend/MessageModel.py
+++ Backend/MessageModel.py
@@ -1,6 +1,13 @@
 class MessageModel:
     def __init__(self, x, y):
-        self.message = {
+        self.input = {
             'x': x,
             'y': y
+        }
+
+        self.output = {
+            'prm_mtr': '',
+            'prm_mtr_spd': 0,
+            'sec_mtr': '',
+            'sec_mtr_spd': 0
         }
--- Backend/SlimJimServer.py
+++ Backend/SlimJimServer.py
@@ -18,28 +18,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 rm = rem_gpio()
-rm.remote_gpio_init()
+#rm.remote_gpio_init()
 
 
 @socketio.on('message')
 def handle(msg):
     print(msg)
     print()
-    x_axis = 0
-    primary_motor = ''
-    secondary_motor = ''
-    velocity = 0
-    for i in range(len(msg)):
-        msg_obj = MessageModel(msg[i]['x'], msg[i]['y'])
-        print("x: " + str(m_msg.message['x']) + " y: " + str(m_msg.message['y']))
-        if msg_obj.message['x'] >= 0:
-            x_axis = True
-            primary_motor = 'right'
-        else:
-            x_axis = False
-            primary_motor = 'left'
 
-        rm.
+    for i in range(len(msg)): #For every value recieved from the client
+        msg_obj = MessageModel(msg[i]['x'], msg[i]['y'])
+        print("x: " + str(msg_obj.input['x']) + " y: " + str(msg_obj.input['y']))
+        
+        if msg_ojb.input['x'] >= 0: # Assigning motor priority to be used later
+            msg_obj.output['prm_mtr'] = 'left'
+            msg_obj.output['sec_mtr'] = 'right'
+        else:
+            msg_obj.output['prm_mtr'] = 'right'
+            msg_obj.output['sec_mtr'] = 'left'
+
+        # To make clear what each part does, I've included the parts of the circle that are solved with each condition.
+        if abs(msg_obj.input['x']) <= abs(msg_obj.input['y']): # D. 2,3,6,7
+            msg_obj.output['prm_mtr_spd'] = msg_obj.input['y']
+            msg_obj.output['sec_mtr_spd'] = msg_obj.input['y'] - msg_obj.input['x']
+        else:
+            if msg_obj.input['y'] >= 0: #D. 1,4
+                msg_obj.output['prm_mtr_spd'] = msg_obj.input['y']
+                msg_obj.output['sec_mtr_spd'] = abs(msg_obj.input['x'])
+            else:
+                if msg_obj.input['x'] >= 0: #D. 8
+                    msg_obj.output['prm_mtr_spd'] = msg_obj.input['y']
+                    msg_obj.output['sec_mtr_spd'] = -(msg_obj.input['x'])
+                else: #D. 5
+                    msg_obj.output['prm_mtr_spd'] = msg_obj.input['y']
+                    msg_obj.output['sec_mtr_spd'] = msg_obj.input['x']
+
+        print("prm_mtr: " + str(msg_obj.output['prm_mtr_spd']) + " sec_mtr: " + str(msg_obj.output['sec_mtr_spd'])) 
 
         #print(str())
     #value = int(msg['velocity']) / 100
